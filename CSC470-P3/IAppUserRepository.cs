0f41f90bca7f9a855a1370739fe1fa75f806345e
Kyle <Kyle@DESKTOP-NCTN43V>
2021-09-22 17:39:41 -0500

    Created IAppUserRepository

---
+++ CSC470-P3/IAppUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC470_P3
+{
+    public interface IAppUserRepository
+    {
+        bool Login(string UserName, string Password);
+        List<AppUser> GetAll();
+        void SetAuthentication(string UserName, bool IsAuthenticated);
+        AppUser GetByUserName(string UserName);
+    }
+}
