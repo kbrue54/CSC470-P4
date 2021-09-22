b69f4ef672e8ed497a749758486380432031cbfa
Kyle <Kyle@DESKTOP-NCTN43V>
2021-09-22 17:57:28 -0500

    Created FakeAppUserRepository Class and Created a test user

---
+++ CSC470-P3/FakeAppUserRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC470_P3
+{
+    public class FakeAppUserRepository : IAppUserRepository
+    { 
+
+        private static Dictionary<int, AppUser> AppUsers;
+
+        public FakeAppUserRepository()
+        {
+            if (AppUsers == null)
+            {
+                AppUsers = new Dictionary<int, AppUser>();
+                AppUsers.Add(1, new AppUser
+                {
+                    UserName = "kbrue54",
+                    Password = "Password123",
+                    FirstName = "Kyle",
+                    LastName = "Bruening",
+                    EmailAddress = "kyle.bruening@trojans.dsu.edu"
+
+
+                    
+
+                });     
+            }
+
+
+        }
+
+
+
+
+    }
+}
